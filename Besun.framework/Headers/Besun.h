123ed4bbd562e515b7b1ab73e47ab5c1b8213f81
hbxfnsm <80470324@qq.com>
2020-12-30 15:17:03 +0800

    初始化

---
+++ Besun.framework/Headers/Besun.h
@@ -0,0 +1,22 @@
+//
+//	Besun.h
+//
+//	,-. ,-. ,-,-.
+//	| | `-. | | |
+//	' ' `-' ' ' '
+//	Created on 2020/12/30.
+//  
+//
+    
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Besun.
+FOUNDATION_EXPORT double BesunVersionNumber;
+
+//! Project version string for Besun.
+FOUNDATION_EXPORT const unsigned char BesunVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Besun/PublicHeader.h>
+
+
---
+++ Besun.framework/Headers/BesunHelper.h
@@ -0,0 +1,20 @@
+//
+//	BesunHelper.h
+//
+//	,-. ,-. ,-,-.
+//	| | `-. | | |
+//	' ' `-' ' ' '
+//	Created on 2020/12/30.
+//  
+//
+    
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BesunHelper : NSObject
+- (void)test;
+@end
+
+NS_ASSUME_NONNULL_END
